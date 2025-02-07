151eaed923a1dd5092ab6f8caae1ebbc4162d500
quentinTab <67987534+quentinTab@users.noreply.github.com>
2025-02-07 18:43:55 -0500

    Update streamlit_app.py
    connected changes needed bc of move out from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,7 +11,9 @@ st.write(
 
 
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 name_on_order = st.text_input('Name on Smoothie')
 if name_on_order:
     st.write('The name on the Smoothie will be:', name_on_order)
