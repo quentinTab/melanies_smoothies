d73082964c8c0817924227ca453e2d711261cc5e
quentinTab <67987534+quentinTab@users.noreply.github.com>
2025-02-07 19:09:20 -0500

    Update streamlit_app.py
    remove max_selections=5

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
-    ,max_selections=5
 )
-st.write(len(ingredients_list))
-st.stop()
 
 if ingredients_list:
 
